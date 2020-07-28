d423f6c62fa859246a8616c2bf3d900a76de58db
enordquist <erikbnordquist@gmail.com>
2020-07-28 11:17:25 -0400

    forward and reverse added

--- score.py
+++ score.py
@@ -1,5 +1,5 @@
 '''
-python score.py -i input [-o output]
+python score.py [-i input] [-o output] [-d] [-h]
 
 for scoring peptides not comparing 13mer peptide array labels
 Matrix is Sites(5) x Residues(20) x Terms(6)
@@ -27,20 +27,20 @@ residuesDict = {'E':0,'D':1,'K':2,'R':3,'Q':4,'N':5,'P':6,
                 'H':7,'T':8,'S':9,'G':10,'A':11,'V':12,'M':13,
                 'C':14,'I':15,'L':16,'Y':17,'F':18,'W':19}
 
-wsite=[0.2, 0.6, 1.0, 0.4, 0.2]
+wsite=[0.25, 0.50, 1.0, 0.25, 0.25]
 wterm0=[0.20, 0.60, 0.0, 0.0, 0.0, 1.0, 0.40]
 wterm_out=wterm0
 
 ####
 #load params from numpy file rq
 params=np.load('params.npy',allow_pickle=True)
-np.set_printoptions(precision=1,linewidth=80)
+#np.set_printoptions(precision=1,linewidth=80)
 
 # model form:
 # E(XXXXX) = Sum_site( w_site Sum_term( w_term E_{aa,term,site}  )  )
 
-tmpparam = []
-def score(fivemer):
+def score_fivemer(fivemer):
+  tmpparam = []
   try:
     score = 0
     for ws,siteIdx,residue in zip(wsite,site_index,fivemer):
@@ -50,21 +50,53 @@ def score(fivemer):
       if siteIdx is 2: wterms=wterm0
       else: wterms=wterm_out
       for wt,termIdx in zip(wterms,term_index):
-        score_site += wt * tmpparam[termIdx]
+        termij = wt * tmpparam[termIdx]
+        score_site += termij
       score += ws * score_site
     return score
   except KeyError:
-    print(fivemer,'has an unnatural amino acid')
+    print(fivemer,'has some weird input')
 
-def score_peptide(seq):
+def reverse(seq):
+  return seq[::-1]
+
+def score_peptide(seq,detail_set):
   seq_score = []
   for i in range(0,len(seq)-4):
-    pep = seq[i:int(i+5)]
-    s = score(pep)
-    seq_score.append([pep,s])
+    fivemer= seq[i:int(i+5)]
+    if detail_set:
+      score, details = detail(fivemer)
+    else:
+      score = score_fivemer(fivemer)
+      rscore = score_fivemer(reverse(fivemer))
+      details = ''
+      seq_score.append([fivemer,score,rscore,details])
   df = pd.DataFrame(seq_score)
-  df.columns = ['seq','score']
+  df.columns = ['seq','score','reverse-score','detail']
   return df 
+
+def detail(fivemer):
+  details = {}
+  try:
+    score = 0
+    for ws,siteIdx,residue in zip(wsite,site_index,fivemer):
+      terms = {}
+      score_site = 0
+      resIdx=residuesDict[residue] #returns matrix index of residue I
+      tmpparam = params[siteIdx][resIdx]
+      if siteIdx is 2: wterm = wterm0
+      else: wterm=wterm_out
+      for wt,termIdx in zip(wterm,term_index):
+        t = ws * wt * tmpparam[termIdx]
+        terms[termsDict[termIdx]] = t
+        score_site += t
+      details["site "+str(siteIdx-2)] = terms
+      score += score_site
+    return score, pd.DataFrame(details)
+
+  except KeyError:
+    print(fivemer,'has some weird input')
+
 
 def read_fasta(ifile):
   Pepdict={}
@@ -76,34 +108,39 @@ def read_fasta(ifile):
       Pepdict[key] = line
   return Pepdict
 
-def write_scores(sequences,ofile):
+def get_scores(fasta,detail):
   out={}
-  for name,seq in sequences.items():
-    scores = score_peptide(seq)  
-    print(scores)
-    out[name] = scores
-  #print(out)
-  out = pd.DataFrame.from_dict(out,orient='index',columns=['names'])
-  out.to_csv(ofile)
+  for name,seq in fasta.items():
+    out[name] = score_peptide(seq,detail)
+  return pd.concat(out)
+  
+def print_summary(scores):
+  print('SUMMARY')
+  print(scores)
 
-
+def write_scores(scores,ofile):
+  out = pd.DataFrame.from_dict(df)
+  out.to_csv(ofile,float_format='%.2f')
 
 def main():
   # cl arguments
-  ifile='test.fasta'
-  ofile=ifile+'.out'
-  parser = argparse.ArgumentParser(description='calculate scores for peptides')
-  parser.add_argument('-i','--infile',default=ifile,dest='IFile',help='source of peptides to be scored')
-  parser.add_argument('-o','--outfile',default=ofile,dest='OFile',help='destination of scores')
-  parser.add_argument('-d','--detail',action='store_true',dest='detail',help='flag to give detailed score breakdown')
+  parser = argparse.ArgumentParser(description='calculate scores for peptides saved in fasta format')
+  parser.add_argument('-i','--infile',required=True,dest='IFile',help='fasta format, source of peptides to be scored')
+  parser.add_argument('-o','--outfile',             dest='OFile',help='destination of scores')
+  parser.add_argument('-d','--detail',action='store_true',dest='Detail',help='flag to give detailed score breakdown')
+  parser.add_argument('-q','--quiet',action='store_true',dest='Quiet',help='flag to suppress summary ouput printed to screen')
   args=parser.parse_args()
   
   # read fasta-format input file
   # fasta format:
   #>name
   #SEQUENCE
-  sequences=read_fasta(ifile)
-  write_scores(sequences,ofile)
+
+  fasta=read_fasta(args.IFile)
+  scores=get_scores(fasta,args.Detail)
+  if not args.Quiet: print_summary(scores)
+  if args.OFile is not None: write_scores(scores,args.OFile)
+  if args.Quiet and args.OFile is None: print('use -o outputfile to save output to text file')
 
 if __name__ == "__main__":
   main()
